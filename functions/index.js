459bced2dee4ced9bffebe102a8c99eca503d618
cryptoxmamx <dineshpateliya1979@gmail.com>
2026-04-26 22:35:53 +0530

    Fix python3 error by downloading standalone yt-dlp_linux binary

--- functions/index.js
+++ functions/index.js
@@ -221,7 +221,7 @@ async function ensureYtDlpBinary() {
         ? 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp.exe'
         : process.platform === 'darwin'
           ? 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_macos'
-          : 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp'
+          : 'https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp_linux'
 
     const response = await fetch(downloadUrl)
 
